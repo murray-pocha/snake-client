67227b9ea426188c7a76e72a5a50df0dd18be8c5
murray-pocha <murray.pocha@gmail.com>
2024-10-23 17:14:26 -0600

    checking code

--- client.js
+++ client.js
@@ -9,18 +9,16 @@ const connect = function() {
   });
 
   conn.on('connect', () => {
-    console.log('Welcome to the server!');
+    console.log('Connected, Welcome to the server!');
     conn.write('Name: MDP');
-    //const moveUpInterval = setInterval(() => {
-    //conn.write('Move: up');
-    //}, 50);
-
 
   });
 
   conn.on('data', (data) => {
     console.log(data);
   });
+
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
