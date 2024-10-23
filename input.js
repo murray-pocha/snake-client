46434377c1eaf2173a66c8366b39a4ed51d67698
murray-pocha <murray.pocha@gmail.com>
2024-10-23 15:21:35 -0600

    added movements in the handleUserInput function, made connection to server

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -7,10 +10,24 @@ const setupInput = function() {
   return stdin;   // return the stdin object so we can use it elsewhere in the program
 };
 
+
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log('Exiting program.');
     process.exit();
+  }
+
+  if (key === 'w') {
+    connection.write('Move: up');
+
+  } else if (key === 'a') {
+    connection.write('Move: left');
+
+  } else if (key === 's') {
+    connection.write('Move: down');
+
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
