b9941143f1ca9b1bce8e1b529a678524b28e9416
murray-pocha <murray.pocha@gmail.com>
2024-10-23 17:19:41 -0600

    cleaned up code

--- input.js
+++ input.js
@@ -2,12 +2,12 @@ let connection;
 
 const setupInput = (conn) => {
   connection = conn;
-  const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
-  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
-  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
-  stdin.resume(); // resume stdin so the program can listen for input
-  stdin.on('data', handleUserInput); // listens to the users input (registered by the .on)
-  return stdin;   // return the stdin object so we can use it elsewhere in the program
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
 };
 
 const movementprompts = {
