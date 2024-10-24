53e8422473e7d3593adf7c73870c00ff00d9c551
murray-pocha <murray.pocha@gmail.com>
2024-10-24 11:10:10 -0600

    added constants and cleaned code

--- constants.js
+++ constants.js
@@ -1,8 +1,24 @@
 const IP = "localhost";
 const PORT = 50541;
 
+const movementprompts = {
+  'w': 'up',
+  'a': 'left',
+  's': 'down',
+  'd': 'right'
+};
+
+const specialMessage = {
+  'j': 'Say: Hey, everyone!',
+  'k': 'Say: I win!',
+  'l': 'Say: ahhhhh, I lost'
+};
+
+
 module.exports = {
   IP,
   PORT,
+  movementprompts,
+  specialMessage
 };
 
